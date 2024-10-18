52e47a09ac90f7936e3c3fb97d3165019bc6cceb
Krishang <krishang@thirdweb.com>
2024-10-18 16:48:53 +0530

    Add benchmark tests

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 # Define the targets
 .PHONY: test_all
 
-test: test_fork test_invariant
+test: test_fork test_invariant test_benchmark
 
 test_fork:
 	@forge test --mc Fork --fork-url $(FORK_URL)
 
 test_invariant:
 	@forge test --mc Invariant
+
+test_benchmark:
+	@forge test --mc Benchark --fork-url $(FORK_URL)
---
+++ test/DoxaBenchmarkTest.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DoxaBondingCurve} from "src/DoxaBondingCurve.sol";
+import {DoxaFactory} from "src/DoxaFactory.sol";
+
+contract DoxaBenchmarkTest is Test {
+
+    DoxaFactory public factory;
+    DoxaBondingCurve public bondingCurve;
+
+    function setUp() public {
+        factory = new DoxaFactory();
+        bondingCurve = DoxaBondingCurve(factory.createToken("MyToken", "TKN"));
+    }
+
+    function test_benchmark_createToken() public {
+        vm.pauseGasMetering();
+        DoxaFactory fac = factory;
+        vm.resumeGasMetering();
+        fac.createToken("MyToken", "TKN");
+    }
+
+    function test_benchmark_buy_oneEther() public {
+        vm.pauseGasMetering();
+        DoxaBondingCurve bc = bondingCurve;
+        vm.resumeGasMetering();
+        bc.buy{value: 1 ether}();
+    }
+
+    function test_benchmark_buy_lessThanOneEther() public {
+        vm.pauseGasMetering();
+        DoxaBondingCurve bc = bondingCurve;
+        vm.resumeGasMetering();
+        bc.buy{value: 0.5 ether}();
+    }
+
+    function test_benchmark_buy_betweenOneAndTwoEther() public {
+        vm.pauseGasMetering();
+        DoxaBondingCurve bc = bondingCurve;
+        vm.resumeGasMetering();
+        bc.buy{value: 1.5 ether}();
+    }
+
+    function test_benchmark_buy_maxEther() public {
+        vm.pauseGasMetering();
+        DoxaBondingCurve bc = bondingCurve;
+        vm.resumeGasMetering();
+        bc.buy{value: 10 ether}();
+    }
+
+    function test_benchmark_buyback() public {
+        vm.pauseGasMetering();
+        DoxaBondingCurve bc = bondingCurve;
+
+        for(uint i = 0; i < 101; i++) {
+            bc.buy{value: 1 ether}();
+        }
+        vm.resumeGasMetering();
+        bc.buyback();
+    }
+}
